c07fd5fddc34f43ba3fd4e5e72fae3d5df1dbe82
ibrahimkahraman <ikahraman1938@gmail.com>
2025-03-11 10:12:58 +0300

    wachat kontrolü yapıldı

--- lib/splash/screen/splash_screen.dart
+++ lib/splash/screen/splash_screen.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wadual/providers/premium_provider.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends ConsumerWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isPremium = ref.watch(isPremiumProvider);
+
     Future.delayed(
       const Duration(seconds: 2),
       () async {
         final prefs = await SharedPreferences.getInstance();
         final isFirstLaunch = prefs.getBool('isFirstLaunch') ?? false;
-        if (isFirstLaunch) {
-          context.go('/home');
+        if (!isPremium) {
+          if (isFirstLaunch) {
+            context.go('/home');
+          } else {
+            context.go('/onboarding');
+          }
         } else {
-          context.go('/onboarding');
+          context.go('/wachat');
         }
       },
     );
